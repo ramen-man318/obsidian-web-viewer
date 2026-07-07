0e5a669c416ddaa42816ca6ca2d23b848e8303b2
Luna-chan <luna@lunamen-dou.com>
2026-07-08 01:29:41 +0900

    セキュリティ修正 & README更新

--- app.py
+++ app.py
@@ -173,4 +173,5 @@ def index():
     return send_from_directory('.', 'index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    debug = os.environ.get('FLASK_DEBUG', '0') == '1'
+    app.run(host='0.0.0.0', port=5000, debug=debug)
